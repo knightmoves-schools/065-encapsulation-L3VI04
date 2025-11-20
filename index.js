1d9afc983b50d1642623227cfb1ced89453aa69f
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-11-20 13:32:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// index.js
-
 class Rectangle {
   constructor(length, height) {
-    // Encapsulated variables
-    this.width = length;
+    this.length = length;
     this.height = height;
   }
-
-  // Method to calculate area (no arguments needed)
+  
   calculateArea() {
     return this.length * this.height;
   }
@@ -19,6 +15,7 @@ const height =  10;
 const rect = new Rectangle(length, height)
 
 console.log(rect.calculateArea());
+
 
 
 
